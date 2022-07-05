69f95ec0ccf4dafac98168ec546344591b6447bb
Ryota Oshima <ryota.oshima@rakus-partners.co.jp>
2022-07-05 10:15:21 +0900

    first commit

---
+++ src/main/java/com/example/demo/ExEmpManagement1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExEmpManagement1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExEmpManagement1Application.class, args);
+	}
+
+}
